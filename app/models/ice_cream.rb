89476746855c16e637ce9ccdbfbc01d590242071
RamonaEJSpence <ramonaejspence@gmail.com>
2020-09-22 20:14:44 -0400

    installed RSpec, passed model validation tests

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -1,2 +1,7 @@
 class IceCream < ApplicationRecord
+    validates :name, presence: true, 
+    length: { minimum: 10 }
+    validates :description, presence: true,
+    length: { minimum: 25 }
+    validates :date_made, presence: true
 end
